4290002dd6ea138895fa200eaad4971c39ac8a79
Sergio San Pedro <sergio.sanpedro@me.com>
2019-04-04 10:39:14 -1000

    Hackwichseven complete

---
+++ HackwichSeven/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  Restaurant.swift
+//  HackwichSeven
+//
+//  Created by Sergio San Pedro on 4/4/19.
+//  Copyright © 2019 Sergio San Pedro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class Restaurant: NSObject, MKAnnotation {
+
+    let restaurantTitle: String?
+    let restaurantType: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, type: String, coordinate: CLLocationCoordinate2D) {
+        self.restaurantTitle = title
+        self.restaurantType = type
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return restaurantTitle
+    }
+}
--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
+    
+    let regionRadius: CLLocationDistance = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        centerMapOnLocation(location: initialLocation)
+        
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate:CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.0800))
+        mapView.addAnnotation(restaurantOne)
+       
+        }
 
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+       
+        
+    }
+    
 
 }
+
 
